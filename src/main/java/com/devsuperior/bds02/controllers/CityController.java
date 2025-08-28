1d6c850e8ff7a91f8bd8571dadacddd3d9e51540
conradrenno <conradrenno@hotmail.com>
2025-08-28 15:25:21 -0300

    Adding controller, service, repository and defining findAll method that matches with tdd specification

---
+++ src/main/java/com/devsuperior/bds02/controllers/CityController.java
@@ -0,0 +1,25 @@
+package com.devsuperior.bds02.controllers;
+
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.services.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cities")
+public class CityController {
+
+    @Autowired
+    CityService service;
+
+    @GetMapping
+    public ResponseEntity<List<CityDTO>> findAll() {
+        List<CityDTO> cities = service.findAll();
+        return ResponseEntity.ok().body(cities);
+    }
+}
---
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -0,0 +1,24 @@
+package com.devsuperior.bds02.services;
+
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.repositories.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class CityService {
+
+    @Autowired
+    private CityRepository repository;
+
+    @Transactional
+    public List<CityDTO> findAll() {
+        List<City> cities = repository.findAll(Sort.by("name"));
+        return cities.stream().map(x -> new CityDTO(x)).toList();
+    }
+}
